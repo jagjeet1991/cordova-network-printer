7ac7a8b98370bb0a0a66d88659cb1cc1815146de
Baljinder Singh <baljindersingh@Baljinders-MacBook-Pro.local>
2019-03-18 17:35:44 +0530

    Bug Fix

--- src/android/NetworkPrinter.java
+++ src/android/NetworkPrinter.java
@@ -7,9 +7,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.util.Log;
+
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.io.DataOutputStream;
+import java.net.UnknownHostException;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -17,12 +20,13 @@ import java.io.DataOutputStream;
 public class NetworkPrinter extends CordovaPlugin {
 
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        /*if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        }*/
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
+            throws JSONException {
+    /*if (action.equals("coolMethod")) {
+        String message = args.getString(0);
+        this.coolMethod(message, callbackContext);
+        return true;
+    }*/
         if (action.equals("print")) {
             this.print(args, callbackContext);
             return true;
@@ -30,44 +34,56 @@ public class NetworkPrinter extends CordovaPlugin {
         return false;
     }
 
-    /*private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
-    }*/
+  /*private void coolMethod(String message, CallbackContext callbackContext) {
+      if (message != null && message.length() > 0) {
+          callbackContext.success(message);
+      } else {
+          callbackContext.error("Expected one non-empty string argument.");
+      }
+  }*/
 
-    private void print(JSONArray args, CallbackContext callbackContext) {
+    private void print(JSONArray args, final CallbackContext callbackContext) {
         if (args != null) {
-            try {
-                String ip = args.getJSONObject(0).getString("ip");
-                if (ip == null) {
-                    throw new Exception("Network address not found.");
+            Thread thread = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        String ip = args.getJSONObject(0).getString("ip");
+                        if (ip == null) {
+                            throw new Exception("Network address not found.");
+                        }
+                        int port = Integer.parseInt(args.getJSONObject(0).getString("port"));
+        /*if (port == 0 == null) {
+            throw new Exception("IP port not found.");
+        }*/
+                        String content = args.getJSONObject(0).getString("content");
+                        if (content == null) {
+                            throw new Exception("Print content not found.");
+                        }
+
+                        Socket socket = new Socket(ip, port);
+                        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                        dataOutputStream.write(content.getBytes());
+                        dataOutputStream.close();
+                        socket.close();
+
+                        callbackContext.success("Success");
+                    } catch (UnknownHostException ex) {
+                        Log.v("Unknown Error", String.valueOf(ex));
+                        callbackContext.error("Error");
+                    } catch (IOException ex) {
+                        Log.v("IO Exception Error", String.valueOf(ex));
+                        callbackContext.error("Error");
+                    } catch (Exception ex) {
+                        Log.v("Exception", String.valueOf(ex));
+                        callbackContext.error("Error");
+                    }
                 }
-                int port = Integer.parseInt(args.getJSONObject(0).getString("port"));
-                /*if (port == 0 == null) {
-                    throw new Exception("IP port not found.");
-                }*/
-                String content = args.getJSONObject(0).getString("content");
-                if (content == null) {
-                    throw new Exception("Print content not found.");
-                }
-
-                Socket socket = new Socket(ip, port);
-                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-
-                dos.write(content.getBytes());
-                dos.close();
-                socket.close();
-
-                callbackContext.success("Success");
-            } catch (Exception ex) {
-                callbackContext.error("Error");
-            }
+            };
+            thread.start();
         } else {
             callbackContext.error("Expected parameters not found.");
         }
     }
-
 }
