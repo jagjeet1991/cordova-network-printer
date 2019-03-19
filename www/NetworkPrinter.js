601f1929e146f84933ace44f9d11c64b9ea91eae
Baljinder Singh <baljindersingh@Baljinders-MacBook-Pro.local>
2019-03-19 09:52:29 +0530

    Minor Fix

--- www/NetworkPrinter.js
+++ www/NetworkPrinter.js
@@ -1,9 +1,10 @@
 var exec = require('cordova/exec');
 
-module.exports.coolMethod = function (arg0, success, error) {
+/*module.exports.coolMethod = function (arg0, success, error) {
  exec(success, error, 'NetworkPrinter', 'coolMethod', [arg0]);
-};
+};*/
 
-module.exports.print = function (arg0, success, error) {
+exports.print = function (arg0, success, error) {
+ alert('adfdfsd');
  exec(success, error, 'NetworkPrinter', 'print', [arg0]);
 }
