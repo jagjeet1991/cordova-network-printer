9e8524b6c36ccf13d96fca72f79828d718128088
Baljinder Singh <baljindersingh@Baljinders-MacBook-Pro.local>
2019-03-21 11:38:41 +0530

    Minor Changes

--- src/android/NetworkPrinter.java
+++ src/android/NetworkPrinter.java
@@ -16,77 +16,80 @@ import java.net.UnknownHostException;
 
 public class NetworkPrinter extends CordovaPlugin {
 
-  @Override
-  public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
-      throws JSONException {
-    if (action.equals("print")) {
-      this.print(args, callbackContext);
-      return true;
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
+            throws JSONException {
+        if (action.equals("print")) {
+            this.print(args, callbackContext);
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
 
-  private void print(JSONArray args, final CallbackContext callbackContext) {
-    if (args != null && args.length() > 0) {
-      Thread thread =
-          new Thread() {
-            @Override
-            public void run() {
-              try {
-                String ip = args.getJSONObject(0).getString("ip");
-                if (ip == null) {
-                  throw new Exception("IP address not found.");
-                }
+    private void print(JSONArray args, final CallbackContext callbackContext) {
+        if (args != null && args.length() > 0) {
+            Thread thread =
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            try {
+                                String ip = null;
+                                if (!args.getJSONObject(0).has("ip")) {
+                                    ip = args.getJSONObject(0).getString("ip");
+                                }
+                                if (ip == null) {
+                                    throw new Exception("IP address not found.");
+                                }
+                                
+                                String portAddr = args.getJSONObject(0).getString("port");
+                                if (portAddr == null) {
+                                    throw new Exception("Port not found.");
+                                }
+                                int port = Integer.parseInt(portAddr);
 
-                String portAddr = args.getJSONObject(0).getString("port");
-                if (portAddr == null) {
-                  throw new Exception("Port not found.");
-                }
-                int port = Integer.parseInt(portAddr);
+                                String content = args.getJSONObject(0).getString("content");
+                                if (content == null) {
+                                    throw new Exception("Print content not found.");
+                                }
 
-                String content = args.getJSONObject(0).getString("content");
-                if (content == null) {
-                  throw new Exception("Print content not found.");
-                }
+                                Socket socket = new Socket(ip, port);
+                                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
-                Socket socket = new Socket(ip, port);
-                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                                dataOutputStream.write(content.getBytes());
+                                dataOutputStream.close();
+                                socket.close();
 
-                dataOutputStream.write(content.getBytes());
-                dataOutputStream.close();
-                socket.close();
-
-                String success = pluginResponse("true", "Printing is done.");
-                callbackContext.success(success);
-              } catch (UnknownHostException ex) {
-                String error = pluginResponse("false", String.valueOf(ex));
-                callbackContext.error(error);
-              } catch (IOException ex) {
-                String error = pluginResponse("false", String.valueOf(ex));
-                callbackContext.error(error);
-              } catch (Exception ex) {
-                String error = pluginResponse("false", String.valueOf(ex));
-                callbackContext.error(error);
-              }
-            }
-          };
-      thread.start();
-    } else {
-      callbackContext.error(pluginResponse("false", "Expected paramerters not found."));
+                                String success = pluginResponse("true", "Printing is done.");
+                                callbackContext.success(success);
+                            } catch (UnknownHostException ex) {
+                                String error = pluginResponse("false", String.valueOf(ex));
+                                callbackContext.error(error);
+                            } catch (IOException ex) {
+                                String error = pluginResponse("false", String.valueOf(ex));
+                                callbackContext.error(error);
+                            } catch (Exception ex) {
+                                String error = pluginResponse("false", String.valueOf(ex));
+                                callbackContext.error(error);
+                            }
+                        }
+                    };
+            thread.start();
+        } else {
+            callbackContext.error(pluginResponse("false", "Expected paramerters not found."));
+        }
     }
-  }
 
-  public String pluginResponse(String status, String message) {
-    String response = "";
-    try {
-      Log.v("Network Printer Message", message);
-      JSONObject res = new JSONObject();
-      res.put("status", status);
-      res.put("message", message);
-      response = res.toString();
-    } catch (Exception ex) {
-      // ex.printStackTrace();
+    public String pluginResponse(String status, String message) {
+        String response = "";
+        try {
+            Log.v("Network Printer Message", message);
+            JSONObject res = new JSONObject();
+            res.put("status", status);
+            res.put("message", message);
+            response = res.toString();
+        } catch (Exception ex) {
+            // ex.printStackTrace();
+        }
+        return response;
     }
-    return response;
-  }
 }
